52922e8fc2f537d3c681d46f7aea57e918317212
Ashley Dabb <ashleyadabb@gmail.com>
2024-02-10 16:06:13 -0700

    working code lab4

--- Labs/Lab4/my_proj/my_app/my_objects.py
+++ Labs/Lab4/my_proj/my_app/my_objects.py
@@ -6,23 +6,27 @@ class vehicle():
     self.year = year
     self.color = color
     self._sound = sound
-
-    def __honk__(self):
-        return self._sound
-    
-    @property 
-    def wheels(self):
-        return self._wheels
-    
+ 
 class Car(vehicle):
     def __init__(self, make, model, year, color, sound):
        super().__init__(make, model, year, color, sound)
        
        self._wheels = 4
+       
+    def honk(self):
+       return self._sound
+    
+    @property 
+    def wheels(self):
+        return self._wheels
 
 class motorcyle(vehicle):
     def __init__ (self, make, model, year, color, sound):
         super().__init__(make, model, year, color, sound)
 
         self._wheels = 2 
+    
+    @property 
+    def wheels(self):
+        return self._wheels
         
